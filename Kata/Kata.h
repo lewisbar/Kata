81d60901a51f6c3e169e57785aa5ee94d681f677
Lennart Wisbar <lewisbar.git@gmail.com>
2025-08-18 14:51:00 +0200

    Initial Commit

---
+++ Kata/Kata.h
@@ -0,0 +1,18 @@
+//
+//  Kata.h
+//  Kata
+//
+//  Created by LennartWisbar on 18.08.25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Kata.
+FOUNDATION_EXPORT double KataVersionNumber;
+
+//! Project version string for Kata.
+FOUNDATION_EXPORT const unsigned char KataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Kata/PublicHeader.h>
+
+
---
+++ KataTests/KataTests.swift
@@ -0,0 +1,17 @@
+//
+//  KataTests.swift
+//  KataTests
+//
+//  Created by LennartWisbar on 18.08.25.
+//
+
+import Testing
+@testable import Kata
+
+struct KataTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
