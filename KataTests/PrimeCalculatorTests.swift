5b135d28c8b85c1a6ac6915a9c7a0ed43eaba221
Lennart Wisbar <lewisbar.git@gmail.com>
2025-08-18 15:00:47 +0200

    Primes up to 1 returns empty array

---
+++ KataTests/PrimeCalculatorTests.swift
@@ -0,0 +1,22 @@
+//
+//  PrimeCalculatorTests.swift
+//  KataTests
+//
+//  Created by LennartWisbar on 18.08.25.
+//
+
+import Testing
+import Kata
+
+enum PrimeCalculator {
+    static func primes(upTo upperBound: Int) -> [Int] {
+        []
+    }
+}
+
+struct PrimeCalculatorTests {
+    @Test func primesUpTo1_returnsEmpty() {
+        #expect(PrimeCalculator.primes(upTo: 1) == [])
+    }
+
+}
