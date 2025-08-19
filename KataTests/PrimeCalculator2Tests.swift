785cb25b7517ca94c3a0e4f72e09568adf29dc2f
Lennart Wisbar <lewisbar.git@gmail.com>
2025-08-19 15:59:29 +0200

    Returns empty for negative upper bounds

---
+++ KataTests/PrimeCalculator2Tests.swift
@@ -0,0 +1,20 @@
+//
+//  PrimeCalculator2Tests.swift
+//  KataTests
+//
+//  Created by LennartWisbar on 19.08.25.
+//
+
+import Testing
+
+enum PrimeCalculator2 {
+    static func calculatePrimes(upTo upperBound: Int) -> [Int] {
+        []
+    }
+}
+
+struct PrimeCalculator2Tests {
+    @Test func calculatePrimes_withNegativeUpperBound_returnsEmpty() {
+        #expect(PrimeCalculator2.calculatePrimes(upTo: -1) == [])
+    }
+}
