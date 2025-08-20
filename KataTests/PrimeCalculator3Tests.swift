3bacdb36f0eeae6b33a5cbf2bc779b83d0829854
Lennart Wisbar <lewisbar.git@gmail.com>
2025-08-20 20:02:14 +0200

    Improve performance by only checking know prime up to the square root of the candidate

--- KataTests/PrimeCalculator3Tests.swift
+++ KataTests/PrimeCalculator3Tests.swift
@@ -6,6 +6,7 @@
 //
 
 import Testing
+import Kata
 
 enum PrimeCalculator3 {
     static func primes(upTo upperBound: Int) -> [Int] {
@@ -26,7 +27,11 @@ enum PrimeCalculator3 {
 
 private extension Int {
     func isNextPrime(after allLowerPrimes: [Int]) -> Bool {
+        let root = Int(sqrt(Double(self)))
+
         for lowerPrime in allLowerPrimes {
+            guard lowerPrime <= root else { return true }
+            
             if self % lowerPrime == 0 {
                 return false
             }
@@ -94,5 +99,13 @@ struct PrimeCalculator3Tests {
 
     @Test func primesUpToThirteen_returnsCorrectNumbers() {
         #expect(PrimeCalculator3.primes(upTo: 13) == [2, 3, 5, 7, 11, 13])
+    }
+
+    @Test func primesUpTo_performanceTest() {
+        let startTime = CFAbsoluteTimeGetCurrent()
+        let _ = PrimeCalculator3.primes(upTo: 10_000)
+        let executionTime = CFAbsoluteTimeGetCurrent() - startTime
+
+        #expect(executionTime < 0.04)
     }
 }
