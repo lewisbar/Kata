a1bac3fb5acf4bc2024819bafbeadb2e429e47de
Lennart Wisbar <lewisbar.git@gmail.com>
2025-08-20 19:26:44 +0200

    Return empty for upper bounds 0 and 1

--- KataTests/PrimeCalculator3Tests.swift
+++ KataTests/PrimeCalculator3Tests.swift
@@ -17,4 +17,12 @@ struct PrimeCalculator3Tests {
     @Test func primesUpToNegativeNumber_returnsEmpty() {
         #expect(PrimeCalculator3.primes(upTo: -1) == [])
     }
+
+    @Test func primesUpToZero_returnsEmpty() {
+        #expect(PrimeCalculator3.primes(upTo: 0) == [])
+    }
+
+    @Test func primesUpToOne_returnsEmpty() {
+        #expect(PrimeCalculator3.primes(upTo: 1) == [])
+    }
 }
