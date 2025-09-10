bca2a42eac64734d1971057d18284cc05692d944
Lennart Wisbar <lewisbar.git@gmail.com>
2025-09-10 15:51:33 +0200

    Merge tests to improve readability, now that the algorithm should be correct and the tests are mainly there to catch regressions

--- KataTests/PrimeCalculator9Tests.swift
+++ KataTests/PrimeCalculator9Tests.swift
@@ -37,32 +37,17 @@ private extension Int {
 }
 
 struct PrimeCalculator9Tests {
-    @Test func upperBoundSmallerThan2_returnsEmpty() {
+    @Test func primesUpTo_returnsCorrectPrimes() {
         #expect(calculatePrimes(upTo: -1) == [])
         #expect(calculatePrimes(upTo: 0) == [])
         #expect(calculatePrimes(upTo: 1) == [])
-    }
-
-    @Test func upperBound2_returns2() {
         #expect(calculatePrimes(upTo: 2) == [2])
-    }
-
-    @Test func upperBound3_returnsCorrectNumbers() {
         #expect(calculatePrimes(upTo: 3) == [2, 3])
-    }
-
-    @Test func upperBound4_returnsCorrectNumbers() {
         #expect(calculatePrimes(upTo: 4) == [2, 3])
-    }
-
-    @Test func upperBoundUpTo8_returnsCorrectNumbers() {
         #expect(calculatePrimes(upTo: 5) == [2, 3, 5])
         #expect(calculatePrimes(upTo: 6) == [2, 3, 5])
         #expect(calculatePrimes(upTo: 7) == [2, 3, 5, 7])
         #expect(calculatePrimes(upTo: 8) == [2, 3, 5, 7])
-    }
-
-    @Test func upperBoundUpTo9_returnsCorrectNumbers() {
         #expect(calculatePrimes(upTo: 9) == [2, 3, 5, 7])
     }
 
